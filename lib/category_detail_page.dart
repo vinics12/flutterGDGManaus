cbdb38b67868b1512fb58abc9e4a96b2fdf0fa15
Vinícius Cerqueira Silva <vini.cerqueira12@gmail.com>
2018-08-25 15:41:43 -0400

    Category Detail Page update

--- lib/category_detail_page.dart
+++ lib/category_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/category.dart';
 
+final _padding = new EdgeInsets.all(16.0);
+
 class CategoryDetailPage extends StatefulWidget {
   Category category;
 
@@ -17,40 +19,82 @@ class CategoryDetailPage extends StatefulWidget {
   }
 
   @override
-  State<StatefulWidget> createState() => new  CategoryDetailPageState();
+  State<StatefulWidget> createState() => new CategoryDetailPageState();
 }
 
 class CategoryDetailPageState extends State<CategoryDetailPage> {
-  var _count = 0;
-  var _toggled = false;
 
-  void _onTap(){
-    setState((){
-      _count++;
-    });
-  }
 
-  void _onToggle(toggle){
-    setState((){
-      _toggled = toggle;
-    });
+  Widget createDropDown() {
+    return new Container(
+      margin: _padding,
+      child: new DropdownButtonHideUnderline(
+          child: new ButtonTheme(
+            child: new DropdownButton(items: [
+              new DropdownMenuItem(child: new Text('unit1')),
+              new DropdownMenuItem(child: new Text('unit2'))
+            ], onChanged: null),
+      )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: widget.category.color,
-        title: new Text(widget.category.name),
+    var input = new Container(
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new TextField(
+            keyboardType: TextInputType.number,
+            decoration: new InputDecoration(
+                labelText: "input",
+                border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(0.0)),
+                labelStyle: Theme.of(context).textTheme.body2),
+          ),
+          createDropDown()
+        ],
       ),
-      body: new Container(
-        child: new Center(
-          child: new Column( children: <Widget>[
-            new GestureDetector(child: new Text("$_count teste", style: Theme.of(context).textTheme.title,), onTap: ()=>this._onTap(),),
-            new Switch(value: _toggled, onChanged: (toggle)=> this._onToggle(toggle))
-          ],),
-        ),
-      )
     );
+
+    var arrows = new RotatedBox(
+      quarterTurns: 1,
+      child: new Icon(
+        Icons.compare_arrows,
+        size: 40.0,
+      ),
+    );
+
+    var output = new Container(
+      margin: new EdgeInsets.only(top: 16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new InputDecorator(
+            decoration: new InputDecoration(
+                labelText: "input",
+                border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(0.0)),
+                labelStyle: Theme.of(context).textTheme.body2),
+          ),
+          createDropDown()
+        ],
+      ),
+    );
+
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: widget.category.color,
+          title: new Text(widget.category.name),
+        ),
+        body: new ListView(
+          padding: _padding,
+          children: <Widget>[
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[input, arrows, output],
+            )
+          ],
+        ));
   }
 }
